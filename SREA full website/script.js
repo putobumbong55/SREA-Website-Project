1f97e8f6b0c2bd3358add95a73733a678ae124f9
2bluesocks <6mdxfh4cyn@privaterelay.appleid.com>
2026-05-10 22:13:12 +0100

    Update script.js

--- SREA full website/script.js
+++ SREA full website/script.js
@@ -36,3 +36,17 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000);
+}
